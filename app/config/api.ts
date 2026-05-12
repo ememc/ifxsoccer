17bfd9bdbdc74752829c7c70457c494b19a5729c
Miguel Anastacio Velasquez <miguel.anastacio@gmail.com>
2026-05-12 11:41:47 -0500

    add news fixed

--- app/config/api.ts
+++ app/config/api.ts
@@ -4,6 +4,7 @@ export const API_ENDPOINTS = {
     heros: "/heros/",
     herosFallback: "/v1/heros/",
     images: "/v1/images/",
+    news: "/v1/news/",
     videos: "/v1/videos/",
     programs: "/v1/programs/",
 } as const;
